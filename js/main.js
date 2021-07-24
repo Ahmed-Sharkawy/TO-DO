d0bdfee8d97ff41d1c9d7806995bafe6b685b200
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2021-07-25 01:05:05 +0200

    ahmed2

--- js/main.js
+++ js/main.js
@@ -2,31 +2,47 @@ const input_text = document.getElementById("input_text"),
 
     content = document.getElementById("content");
 
+    // start Create item content
 
 function field_test(e){
     "use start";
+
+
+    // Create parent content
 
     let new_div = document.createElement("div"),
         new_d = document.createAttribute("class");
         new_d.value = "content_Child";
 
+    // Create icon content
+
     let new_i = document.createElement("i"),
         new_i_class = document.createAttribute("class");
         new_i_class.value = "fas fa-times i_style";
 
+        // start input field
 
     if (input_text.value == ""){
+        
         alert("You cannot leave the field blank");
-        } else {
+        
+    } else {
             
-            new_div.setAttributeNode(new_d);
-            content.appendChild(new_div).innerHTML = input_text.value;
-            new_i.setAttributeNode(new_i_class);
-            new_div.appendChild(new_i);
-            
-            new_i.onclick = function () {
-                "use start";
-                this.parentElement.style.display = 'none'; 
-            }
+        // Add the value of the input field
+
+        new_div.setAttributeNode(new_d);
+        content.appendChild(new_div).innerHTML = input_text.value;
+
+        // Add the value of the input field
+
+        new_i.setAttributeNode(new_i_class);
+        new_div.appendChild(new_i);
+
+        // Unwanted field removal function
+        
+        new_i.onclick = function () {
+            "use start";
+            this.parentElement.style.display = 'none'; 
+        };
     };
 };
