e76c33bdb76b5af6c7a0627ad4008551a3a37735
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2021-07-25 06:32:35 +0200

    ahmed2

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@ const input_text = document.getElementById("input_text"),
 
     content = document.getElementById("content");
 
+    action = document.getElementById("action");
+
+action.onsubmit = function(e){
+    e.preventDefault();
+}
     // start Create item content
 
-function field_test(e){
+function field_test(){
     "use start";
-
 
     // Create parent content
 
@@ -20,6 +24,10 @@ function field_test(e){
         new_i_class = document.createAttribute("class");
         new_i_class.value = "fas fa-times i_style";
 
+        new_I = document.createElement("i");
+        new_I_class = document.createAttribute("class");
+        new_I_class.value = "far fas fa-check i_style";
+
         // start input field
 
     if (input_text.value == ""){
@@ -31,12 +39,17 @@ function field_test(e){
         // Add the value of the input field
 
         new_div.setAttributeNode(new_d);
-        content.appendChild(new_div).innerHTML = input_text.value;
+        content.appendChild(new_div).innerHTML = input_text.value ;
+        document.getElementById("input_text").value = "";
 
         // Add the value of the input field
 
         new_i.setAttributeNode(new_i_class);
         new_div.appendChild(new_i);
+
+        new_I.setAttributeNode(new_I_class);
+        new_div.appendChild(new_I);
+        new_I.style.display = "none"
 
         // Unwanted field removal function
         
@@ -44,5 +57,11 @@ function field_test(e){
             "use start";
             this.parentElement.style.display = 'none'; 
         };
+
+        new_div.onclick = function () {
+            "use start";
+            new_I.style.display = "inline-block";
+        };
+
     };
 };
